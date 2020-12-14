e618b250b0003ac86e0e47d9513e8aa2c2ce7f0c
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2020-12-14 22:44:26 +0300

    edit the styling of nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaBars} from 'react-icons/fa'
+import {IconContext} from 'react-icons/lib'
 import { 
     NavbarContainer, 
     Nav, 
@@ -14,35 +15,52 @@ import {
 
 
 const NavBar = ({ toggle }) => {
+    const [scrollNav, setScrollNav] = useState(false)
+
+    const changeNav = () => {
+        if(window.scrollY >= 80) {
+            setScrollNav(true)
+        } else {
+            setScrollNav(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
+
     return (
         <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">
-                        dolla
-                    </NavLogo>
-                    <MobileIcon onClick={toggle}>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to="about">About</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="discover">Discover</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="services">Services</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="signup">Sign Up</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                    <NavBtn>
-                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
-                    </NavBtn>
-                </NavbarContainer>
-            </Nav>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav scrollNav={scrollNav}>
+                    <NavbarContainer>
+                        <NavLogo to="/">
+                            dolla
+                        </NavLogo>
+                        <MobileIcon onClick={toggle}>
+                            <FaBars />
+                        </MobileIcon>
+                        <NavMenu>
+                            <NavItem>
+                                <NavLinks to="about">About</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="discover">Discover</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="services">Services</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="signup">Sign Up</NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                        <NavBtn>
+                            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                        </NavBtn>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
     )
 }
